dc73a8eb904d46f600e035157588f30976372f68
agenciaschooltech <agenciaschooltech@gmail.com>
2024-07-29 08:43:08 -0300

    settings of deploy, url setting

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -7,9 +7,9 @@ function RouterManager() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Contact />} />
-        <Route path="/:contact" element={<DetailsContact />} />
-        <Route path="/new_contact" element={<NewContact />} />
+        <Route path="/contact-list/" element={<Contact />} />
+        <Route path="/contact-list/:contact" element={<DetailsContact />} />
+        <Route path="/contact-list/new_contact" element={<NewContact />} />
       </Routes>
     </BrowserRouter>
   );
